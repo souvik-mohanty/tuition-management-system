f0e4f51802029a9f09df1becfabfac7be4fc6070
SOUVIK MOHANTY <souvik.mohanty.tech@gmail.com>
2026-02-12 10:02:34 +0530

    file structure finalize

---
+++ src/main/java/com/tuition/saas/subscription/dto/SubscriptionDetailsResponse.java
@@ -0,0 +1,4 @@
+package com.tuition.saas.subscription.dto;
+
+public class SubscriptionDetailsResponse {
+}
---
+++ src/main/java/com/tuition/saas/tenant/entity/TeacherTuitionMapping.java
@@ -0,0 +1,4 @@
+package com.tuition.saas.tenant.entity;
+
+public class TeacherTuitionMapping {
+}
